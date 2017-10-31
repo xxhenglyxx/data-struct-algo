0688c6f2166074c1e2afa1528013570f63e50222
ShadowLegend <shadowlegend@Machintos-HDs-MacBook-Pro.local>
2017-10-31 10:48:50 +0700

    First Array Init

---
+++ array/include/array/Array.cpp
@@ -0,0 +1,6 @@
+
+#include "Array.h"
+
+Array::Array ( const int size ) {
+
+}
---
+++ array/src/main.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>
+
+int main ( int argc, char ** argv ) {
+
+    std::cout << "Hello seth" << std::endl;
+
+    return 0;
+
+}
